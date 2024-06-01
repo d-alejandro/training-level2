4c61acdc23d5aaaddd3c8714d42c9fb59843d421
Alexander Dadyka <sanekokokok@gmail.com>
2024-06-01 22:17:40 +0300

    Add: task_test.go to task #2

--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,38 @@
 package main
+
+import "testing"
+
+func Test_unpackString(t *testing.T) {
+	tests := []struct {
+		argument string
+		expected string
+	}{
+		{
+			argument: "a4bc2d5e",
+			expected: "aaaabccddddde",
+		},
+		{
+			argument: "abcd",
+			expected: "abcd",
+		},
+		{
+			argument: "45",
+			expected: "некорректная строка",
+		},
+		{
+			argument: "",
+			expected: "",
+		},
+		{
+			argument: "QQ3f2d12y",
+			expected: "QQQQffddddddddddddy",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.argument, func(t *testing.T) {
+			if actualString := unpackString(test.argument); actualString != test.expected {
+				t.Errorf(`unpackString("%v") = "%v", expected "%v"`, test.argument, actualString, test.expected)
+			}
+		})
+	}
+}
