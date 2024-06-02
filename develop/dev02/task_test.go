d4cc0270cc7591adedc6e24f489643ca1813221e
Alexander Dadyka <sanekokokok@gmail.com>
2024-06-02 21:37:26 +0300

    Add: Test_unpackStringWithEscapeSymbol to task #2

--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -28,8 +28,39 @@ func Test_unpackString(t *testing.T) {
 			expected: "v",
 		},
 		{
-			argument: "QQ3f2d12y",
-			expected: "QQQQffddddddddddddy",
+			argument: "QQ3f2d9y",
+			expected: "QQQQffdddddddddy",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.argument, func(t *testing.T) {
+			if actualString := unpackString(test.argument); actualString != test.expected {
+				t.Errorf(`unpackString("%v") = "%v", expected "%v"`, test.argument, actualString, test.expected)
+			}
+		})
+	}
+}
+
+func Test_unpackStringWithEscapeSymbol(t *testing.T) {
+	tests := []struct {
+		argument string
+		expected string
+	}{
+		{
+			argument: `qwe\4\5`,
+			expected: "qwe45",
+		},
+		{
+			argument: `qwe\45`,
+			expected: "qwe44444",
+		},
+		{
+			argument: `qwe\\5`,
+			expected: `qwe\\\\\`,
+		},
+		{
+			argument: `Xv3\1\2R1\\3yy\72z3`,
+			expected: `Xvvv12R\\\yy77zzz`,
 		},
 	}
 	for _, test := range tests {
