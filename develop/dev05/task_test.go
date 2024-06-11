ebe27b55f5ed5e125bcd88aa5f2b3c8988d25180
Alexander Dadyka <sanekokokok@gmail.com>
2024-06-12 02:51:48 +0300

    Add: test flags "-A 1 -B 2 -n -F" to develop/dev05/task_test.go

--- develop/dev05/task_test.go
+++ develop/dev05/task_test.go
@@ -25,12 +25,36 @@ func TestNewTextSearch(t *testing.T) {
 				inputArray: []string{
 					"computer 7 Jan 20K",
 					"May 3 Jul  2M",
-					"LAPTOP 7 May 11G",
+					"LAPTOP 7 ^May 11G",
 					"laptop|May 5 Jan 30B 3",
 				},
 			},
 			expected: []string{
 				"\u001B[31mMay\u001B[0m 3 Jul  2M",
+			},
+		},
+		{
+			name: "-A 1 -B 2 -n -F",
+			args: args{
+				flags: &search.TextSearchFlagDTO{
+					RowsAfterFlag:  1,
+					RowsBeforeFlag: 2,
+					FixedFlag:      true,
+					LineNumFlag:    true,
+				},
+				pattern: "^May",
+				inputArray: []string{
+					"computer 7 Jan 20K",
+					"May 3 Jul  2M",
+					"LAPTOP 7 ^May 11G",
+					"laptop|May 5 Jan 30B 3",
+				},
+			},
+			expected: []string{
+				"\u001B[32m1\u001B[0m\u001B[34m-\u001B[0mcomputer 7 Jan 20K",
+				"\u001B[32m2\u001B[0m\u001B[34m-\u001B[0mMay 3 Jul  2M",
+				"\u001B[32m3\u001B[0m\u001B[34m:\u001B[0mLAPTOP 7 \u001B[31m^May\u001B[0m 11G",
+				"\u001B[32m4\u001B[0m\u001B[34m-\u001B[0mlaptop|May 5 Jan 30B 3",
 			},
 		},
 	}
