6ce6f7e58dbb53be10f9a56e028feb1a3053e536
Alexander Dadyka <sanekokokok@gmail.com>
2024-06-13 22:33:45 +0300

    Fix: Cut method to TextCut structure

--- develop/dev06/cut/text_cut.go
+++ develop/dev06/cut/text_cut.go
@@ -1,5 +1,12 @@
 package cut
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type TextCut struct {
 	dto *TextCutFlagDTO
 }
@@ -9,5 +16,50 @@ func NewTextCut(dto *TextCutFlagDTO) *TextCut {
 }
 
 func (receiver *TextCut) Cut(inputRows []string) []string {
-	return nil
+	fields := receiver.convertValuesOfFieldsFlag()
+
+	var outputRows []string
+
+	for _, inputRow := range inputRows {
+		splitRows := strings.Split(inputRow, receiver.dto.DelimiterFlag)
+
+		var outputRow string
+
+		if len(splitRows) > 1 {
+			for index, fieldNumber := range fields {
+				if index == 0 {
+					outputRow = splitRows[fieldNumber]
+					continue
+				}
+				outputRow += receiver.dto.DelimiterFlag + splitRows[fieldNumber]
+			}
+			outputRows = append(outputRows, outputRow)
+			continue
+		}
+
+		if !receiver.dto.SeparatedFlag {
+			outputRows = append(outputRows, inputRow)
+		}
+	}
+
+	return outputRows
+}
+
+func (receiver *TextCut) convertValuesOfFieldsFlag() []int {
+	stringArray := strings.Split(receiver.dto.FieldsFlag, ",")
+
+	var integerArray []int
+
+	for _, value := range stringArray {
+		number, err := strconv.Atoi(value)
+
+		if err != nil {
+			fmt.Println("parameter -f error")
+			os.Exit(1)
+		}
+
+		integerArray = append(integerArray, number-1)
+	}
+
+	return integerArray
 }
