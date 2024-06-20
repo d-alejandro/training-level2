d32dd9d535682df651cfacfd2f3b90507bd77e86
Alexander Dadyka <sanekokokok@gmail.com>
2024-06-20 20:52:43 +0300

    Add: cd commands to Handler structure

--- develop/dev08/cmd/handler.go
+++ develop/dev08/cmd/handler.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"os"
 	"os/exec"
 	"regexp"
 	"strconv"
@@ -19,8 +20,7 @@ func NewHandler(forkExecResultChannel chan<- string) *Handler {
 }
 
 func (receiver *Handler) Execute(commandRow string) (string, error) {
-	switch {
-	case strings.Contains(commandRow, "&"):
+	if strings.Contains(commandRow, "&") {
 		commandStrings := receiver.splitAndTrimString(commandRow)
 
 		if len(commandStrings) != 2 || commandStrings[1] != "" {
@@ -28,7 +28,20 @@ func (receiver *Handler) Execute(commandRow string) (string, error) {
 		}
 
 		return receiver.runForkExecCommand(commandStrings[0])
-	case strings.Contains(commandRow, "|"):
+	} else if strings.Contains(commandRow, "|") {
+		//
+	} else if _, isContain := strings.CutPrefix(commandRow, "cd"); isContain {
+		commandSlice := receiver.splitCommandRow(commandRow)
+
+		if len(commandSlice) != 2 {
+			return "", errors.New("invalid cd command")
+		}
+
+		if err := os.Chdir(commandSlice[1]); err != nil {
+			return "", err
+		}
+
+		return "Ok", nil
 	}
 
 	commandSlice := receiver.splitCommandRow(commandRow)
@@ -51,9 +64,11 @@ func (receiver *Handler) Execute(commandRow string) (string, error) {
 
 func (receiver *Handler) splitAndTrimString(command string) []string {
 	commandStrings := strings.Split(command, "&")
+
 	for index, commandString := range commandStrings {
 		commandStrings[index] = strings.TrimSpace(commandString)
 	}
+
 	return commandStrings
 }
 
@@ -61,6 +76,7 @@ func (receiver *Handler) runForkExecCommand(command string) (string, error) {
 	commandSlice := receiver.splitCommandRow(command)
 
 	path, lookPathErr := exec.LookPath(commandSlice[0])
+
 	if lookPathErr != nil {
 		return "", lookPathErr
 	}
@@ -71,26 +87,29 @@ func (receiver *Handler) runForkExecCommand(command string) (string, error) {
 		return "", forkExecErr
 	}
 
-	go func() {
-		var waitStatus syscall.WaitStatus = 0
-
-		_, waitErr := syscall.Wait4(pid, &waitStatus, 0, nil)
-
-		resultStatus := "Done"
-
-		if waitErr != nil || waitStatus != 0 {
-			resultStatus = "Error"
-		}
-
-		result := fmt.Sprintf("%d %s", pid, resultStatus)
-
-		receiver.forkExecResultChannel <- result
-	}()
+	go receiver.waitForkExecCommandAndSendResult(pid)
 
 	return strconv.Itoa(pid), nil
 }
 
+func (receiver *Handler) waitForkExecCommandAndSendResult(pid int) {
+	var waitStatus syscall.WaitStatus = 0
+
+	_, waitErr := syscall.Wait4(pid, &waitStatus, 0, nil)
+
+	resultStatus := "Done"
+
+	if waitErr != nil || waitStatus != 0 {
+		resultStatus = "Error"
+	}
+
+	result := fmt.Sprintf("%d %s", pid, resultStatus)
+
+	receiver.forkExecResultChannel <- result
+}
+
 func (receiver *Handler) splitCommandRow(commandRow string) []string {
 	regExpr := regexp.MustCompile(`('.*')|(".*")|(\S+)`)
+
 	return regExpr.FindAllString(commandRow, -1)
 }
