5886b62ef6ea34f196043f972fd62fcc67c21751
Alexander Dadyka <sanekokokok@gmail.com>
2024-07-08 14:37:14 +0300

    Fix: LogRequest structure

--- develop/dev11/server/middleware/log_request.go
+++ develop/dev11/server/middleware/log_request.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -19,35 +18,37 @@ func NewLogRequest(handler http.Handler) *LogRequest {
 }
 
 func (receiver *LogRequest) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
-	start := time.Now()
+	timeStart := time.Now()
 
 	receiver.nextHandler.ServeHTTP(responseWriter, request)
 
+	duration := time.Since(timeStart)
+
 	requestHeader, marshalHeaderError := json.MarshalIndent(request.Header, "", "  ")
 	if marshalHeaderError != nil {
-		fmt.Println("marshal error:", marshalHeaderError)
+		log.Println("marshal error:", marshalHeaderError)
 		return
 	}
 
 	parseError := request.ParseForm()
 	if parseError != nil {
-		fmt.Println("parse error:", parseError)
+		log.Println("parse error:", parseError)
 		return
 	}
 
 	requestForm, marshalFormError := json.MarshalIndent(request.Form, "", "  ")
 	if marshalFormError != nil {
-		fmt.Println("marshal error:", marshalFormError)
+		log.Println("marshal error:", marshalFormError)
 		return
 	}
 
 	log.Printf(
-		"\nRequest: %s %s\nRemoteAddress: %s\nHeaders:\n%s\nParameters:\n%s\nTimeoutDuration: %s",
+		"\nRequest: %s %s\nRemoteAddress: %s\nHeaders:\n%s\nParameters:\n%s\nTime: %s",
 		request.Method,
 		request.RequestURI,
 		request.RemoteAddr,
 		string(requestHeader),
 		string(requestForm),
-		time.Since(start),
+		duration,
 	)
 }
