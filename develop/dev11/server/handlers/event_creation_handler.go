e6c6cb47ca9183b3aee1fb3b9900ca51030b36f3
Alexander Dadyka <sanekokokok@gmail.com>
2024-07-08 21:36:18 +0300

    Fix: EventCreationHandler structure

--- develop/dev11/server/handlers/event_creation_handler.go
+++ develop/dev11/server/handlers/event_creation_handler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"d-alejandro/training-level2/develop/dev11/server/presenters"
+	"d-alejandro/training-level2/develop/dev11/server/validators"
 	"fmt"
 	"net/http"
 )
@@ -13,5 +15,18 @@ func NewEventCreationHandler() *EventCreationHandler {
 }
 
 func (receiver *EventCreationHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	errorPresenter := presenters.NewErrorPresenter(responseWriter)
+
+	if err := request.ParseForm(); err != nil {
+		errorPresenter.Present(http.StatusBadRequest, err)
+		return
+	}
+
+	eventRequestValidator := validators.NewEventRequestValidator(request)
+	if err := eventRequestValidator.Validate(); err != nil {
+		errorPresenter.Present(http.StatusBadRequest, err)
+		return
+	}
+
 	fmt.Fprint(responseWriter, "EventCreationHandler")
 }
