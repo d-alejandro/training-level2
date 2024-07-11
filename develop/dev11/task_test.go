4291fc19af7b447ce2351e5fcac777b3f10fbcf7
Alexander Dadyka <sanekokokok@gmail.com>
2024-07-11 22:30:57 +0300

    Add: tests to task #11

--- develop/dev11/task_test.go
+++ develop/dev11/task_test.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"d-alejandro/training-level2/develop/dev11/server"
+	"d-alejandro/training-level2/develop/dev11/server/database"
+	"d-alejandro/training-level2/develop/dev11/server/models"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestEndpoints(t *testing.T) {
+	dbConnection := database.GetDatabaseConnection()
+
+	id := "1"
+	event := &models.Event{
+		ID:   id,
+		Name: "Test Event",
+		Date: time.Now(),
+	}
+	_ = dbConnection.SetEvent(id, event)
+
 	tests := []struct {
 		name                      string
 		method                    string
@@ -48,6 +61,64 @@ func TestEndpoints(t *testing.T) {
 			expectedStatus:            http.StatusBadRequest,
 			expectedHeaderContentType: "application/json",
 		},
+		{
+			name:   "POST /update_event/" + id,
+			method: http.MethodPost,
+			url:    "/update_event/" + id,
+			requestBody: func() *strings.Reader {
+				data := url.Values{}
+				data.Set("name", "Test Name")
+				data.Set("date", "2024-07-11")
+				return strings.NewReader(data.Encode())
+			},
+			expectedBodyRootKey:       `{"result":`,
+			expectedStatus:            http.StatusOK,
+			expectedHeaderContentType: "application/json",
+		},
+		{
+			name:   "POST /delete_event/" + id,
+			method: http.MethodPost,
+			url:    "/delete_event/" + id,
+			requestBody: func() *strings.Reader {
+				return strings.NewReader("")
+			},
+			expectedBodyRootKey:       `{"result":`,
+			expectedStatus:            http.StatusOK,
+			expectedHeaderContentType: "application/json",
+		},
+		{
+			name:   "GET /events_for_day?date=2000-01-01",
+			method: http.MethodGet,
+			url:    "/events_for_day?date=2000-01-01",
+			requestBody: func() *strings.Reader {
+				return strings.NewReader("")
+			},
+			expectedBodyRootKey:       `{"result":`,
+			expectedStatus:            http.StatusOK,
+			expectedHeaderContentType: "application/json",
+		},
+		{
+			name:   "GET /events_for_week?date=2000-01-01",
+			method: http.MethodGet,
+			url:    "/events_for_week?date=2000-01-01",
+			requestBody: func() *strings.Reader {
+				return strings.NewReader("")
+			},
+			expectedBodyRootKey:       `{"result":`,
+			expectedStatus:            http.StatusOK,
+			expectedHeaderContentType: "application/json",
+		},
+		{
+			name:   "GET /events_for_month?date=2000-01-01",
+			method: http.MethodGet,
+			url:    "/events_for_month?date=2000-01-01",
+			requestBody: func() *strings.Reader {
+				return strings.NewReader("")
+			},
+			expectedBodyRootKey:       `{"result":`,
+			expectedStatus:            http.StatusOK,
+			expectedHeaderContentType: "application/json",
+		},
 	}
 
 	for _, test := range tests {
@@ -56,12 +127,13 @@ func TestEndpoints(t *testing.T) {
 			handler := httpServer.InitRequestHandler()
 
 			responseRecorder := httptest.NewRecorder()
+
 			request, _ := http.NewRequest(test.method, test.url, test.requestBody())
 			request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
 			handler.ServeHTTP(responseRecorder, request)
 
 			result := responseRecorder.Result()
-
 			body, err := io.ReadAll(result.Body)
 			if err != nil {
 				t.Error(err)
@@ -74,7 +146,6 @@ func TestEndpoints(t *testing.T) {
 			}
 
 			resultContentType := result.Header.Get("Content-Type")
-
 			if resultContentType != test.expectedHeaderContentType {
 				t.Errorf(
 					"expected header Content-Type %s, actual header Content-Type %s",
@@ -84,13 +155,12 @@ func TestEndpoints(t *testing.T) {
 				return
 			}
 
-			bodyResult := string(body)
-
-			if !strings.HasPrefix(bodyResult, test.expectedBodyRootKey) {
+			resultBody := string(body)
+			if !strings.HasPrefix(resultBody, test.expectedBodyRootKey) {
 				t.Errorf(
 					"expect response body to contain %s, actual response body to contain %s",
 					test.expectedBodyRootKey,
-					string(body),
+					resultBody,
 				)
 			}
 		})
